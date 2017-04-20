8a982857f31d8333d8084700a1dba0b3e29daac6
MutantJedi <mutantjedi@gmail.com>
2017-04-19 19:22:46 -0700

    Initial Commit

---
+++ WGIdenticon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WGIdenticon
+//
+//  Created by Mark Morrill on 2017/04/19.
+//  Copyright © 2017 WeGame Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
